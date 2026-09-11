7b988b3da0162eaa9fdb1e5689266686b4c36ba2
Nida Arshad <nidaarshad120@gmail.com>
2026-09-11 08:15:15 -0700

    Added load conversation function and main ui

--- streamlit_frontend_threading.py
+++ streamlit_frontend_threading.py
@@ -16,6 +16,10 @@ def reset_chat():
     add_thread(st.session_state['thread_id'])
     st.session_state['message_history'] = []
 
+def load_conversation(thread_id):
+    state = chatbot.get_state(config={'configurable': {'thread_id': thread_id}})
+    # Check if messages key exists in state values, return empty list if not
+    return state.values.get('messages', [])
 
 def add_thread (thread_id):
     if thread_id not in st.session_state['chat_thread']:
@@ -27,10 +31,48 @@ def add_thread (thread_id):
 
 st.sidebar.title('LangGraph Chatbot')
 
-st.sidebar.button('New Chat')
+if st.sidebar.button('New Chat')
+      reset_chat()
+
 
 st.sidebar.header('My Conversations')
 
 for thread_id in st.session_state['chat_threads'] [::-1]:
     if st.sidebar.button(str(thread_id)):
         st.session_state['thread-id']=thread_id
+
+
+
+
+
+# loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+
+     # first add the message to message_history
+    with st.chat_message("assistant"):
+        def ai_only_stream():
+            for message_chunk, metadata in chatbot.stream(
+                {"messages": [HumanMessage(content=user_input)]},
+                config=CONFIG,
+                stream_mode="messages"
+            ):
+                if isinstance(message_chunk, AIMessage):
+                    # yield only assistant tokens
+                    yield message_chunk.content
+
+        ai_message = st.write_stream(ai_only_stream())
+
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
